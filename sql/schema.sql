878bb1b36d89a669cd63d43b570534450e875e90
Flre-fly <66842566+Flre-fly@users.noreply.github.com>
2023-04-12 17:24:02 +0900

    기본 SETTING

---
+++ sql/schema.sql
@@ -0,0 +1,8 @@
+drop table member if exists cascade;
+create table member (0),
+                        money integer not n
+                        member_id varchar(1ull default 0,
+                        primary key (member_id)
+);
+insert into member(member_id, money) values ('hi1',10000);
+insert into member(member_id, money) values ('hi2',20000);
