9ad6ca75bf8867a412cbb8c78a9aac1ab975e13d
Flre-fly <66842566+Flre-fly@users.noreply.github.com>
2023-04-13 15:54:09 +0900

    JDBC 수정 JDBC 삭제

--- src/main/java/hello/jdbc/repository/MemberRepositoryV0.java
+++ src/main/java/hello/jdbc/repository/MemberRepositoryV0.java
@@ -11,6 +11,49 @@ import java.util.NoSuchElementException;
 // JDBC - DriverManager 사용해보기
 public class MemberRepositoryV0 {
 
+    public void delete(String memberId) throws SQLException {
+        String sql = "delete from member where member_id=?";
+
+        Connection connection = null;
+        PreparedStatement pstmt = null;
+        log.info("sql: {}", sql);
+
+        try{
+            connection = getConnection();
+            pstmt = connection.prepareStatement(sql);
+            pstmt.setString(1, memberId);
+            int resultSize = pstmt.executeUpdate();
+            log.info("resultSize = {}", resultSize);
+            //반환값 = 영향받은 row수
+        }catch (SQLException e){
+            log.error("db error", e);
+            throw e;
+        }finally {
+            close(connection, pstmt, null);
+        }
+    }
+    public void update(String memberId, int money) throws SQLException {
+        String sql = "update member set money = ? where member_id = ?";
+
+        Connection connection = null;
+        PreparedStatement pstmt = null;
+        log.info("sql: {}", sql);
+        try{
+            connection = getConnection();
+            pstmt = connection.prepareStatement(sql);
+            pstmt.setInt(1, money);
+            pstmt.setString(2, memberId);
+            int resultSize = pstmt.executeUpdate();//sql을 커넥션을 통해 db에 전달
+            log.info("resultSize = {}", resultSize);
+            //반환값 = 영향받은 row수
+        }catch (SQLException e){
+            log.error("db error", e);
+            throw e;//예외 다시 던지기
+        }finally {
+            //쿼리실행 후 자원정리 필요, finally에 작성해야함
+            close(connection, pstmt, null);
+        }
+    }
     public Member findById(String memberId) throws SQLException {
         String sql = "select * from member where member_id = ?";
 
--- src/test/java/hello/jdbc/respository/MemberRepositoryV0Test.java
+++ src/test/java/hello/jdbc/respository/MemberRepositoryV0Test.java
@@ -7,6 +7,7 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.sql.SQLException;
+import java.util.NoSuchElementException;
 
 @Slf4j
 public class MemberRepositoryV0Test {
@@ -15,7 +16,7 @@ public class MemberRepositoryV0Test {
     @Test
     void crud() throws SQLException {
         //save
-        Member member = new Member("a", 10000);
+        Member member = new Member("11121asd", 10000);
         repositoryV0.save(member);
 
         //findById
@@ -24,7 +25,13 @@ public class MemberRepositoryV0Test {
         log.info(" ==비교 결과: {}", findMember==member);
         log.info("isEqual비교결과:  {}", member.equals(findMember));
 
-        //인스턴스비교(==비교)가 아니라 equalto를 사용한 값 비교를 사용했기때문에 true가 나온다
-        Assertions.assertThat(findMember).isEqualTo(member);
-    }
+        //update: money: 10000 -> 20000
+        repositoryV0.update(member.getMember_id(), 20000);
+        Member updatedMember = repositoryV0.findById(member.getMember_id());
+
+        repositoryV0.delete(member.getMember_id());
+
+        Assertions.assertThatThrownBy(()-> repositoryV0.findById(member.getMember_id()))
+                        .isInstanceOf(NoSuchElementException.class);
+   }
 }
